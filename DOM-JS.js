5e72cff8e3a46d3ef7c2a475b687f10bea14b73a
imamf85 <imamfachrudin9@gmail.com>
2022-09-17 09:45:28 +0700

    perubahan terkait data

--- DOM-JS.js
+++ DOM-JS.js
@@ -1,56 +1,33 @@
 fetch('https://jsonplaceholder.typicode.com/todos')
     .then((res) => {
-
-
         if (res.status != 200) {
             console.log('aduuh' + res.status);
-            return
+            return;
         }
 
-
-        res.json()
-            .then((res) => {
+        res.json().then((res) => {
             console.log(res);
-            changeElement(res)
-        }
-            )
-
+            changeElement(res);
+        });
     })
 
     .catch((err) => console.log(err));
 
-let changeElement = (data) => {
+let changeElement = (datas) => {
+    let newEl = '';
+
+    datas.forEach((data) => {
+        newEl += `
+            <div>
+                <span>${data.title}</span>
+                <br />
+                <span style="color: ${data.completed ? 'green' : 'red'}">${
+            data.completed ? 'Complete' : 'Not Complete'
+        }</span>
+            </div>
+        `;
+    });
 
     let areaArticle = document.getElementById('area');
-    let mainDiv = document.createElement('div');
-    let elementFirstChild = document.createElement('span');
-    let elementSecondChild = document.createElement('br');
-    let elementThirdChild = document.createElement('span');
-
-    // areaArticle.innerHTML="";
-    let boxes = []
-
-    for (let i = 0; i < data.length; i++) {
-
-        
-
-        mainDiv.className= 'box';
-        
-        elementFirstChild.innerHTML= data[i].title;
-        elementThirdChild.innerHTML= data[i].completed ? 'Complete' : 'Not complete';
-        
-        elementThirdChild.style.color = data[i].completed ? 'Green' : 'Red';
-
-
-        mainDiv.append(elementFirstChild,elementSecondChild,elementThirdChild);
-        boxes.push(mainDiv)
-        
-    }
-
-    console.log(boxes);
-    areaArticle.append(boxes);
-    
-   
-
-
-}
+    areaArticle.innerHTML = newEl;
+};
